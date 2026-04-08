57494633b67baf7f739cdbfdece50dd621f7683a
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-04-08 23:47:57 +0900

    wrote def of step1_window.py. and then, import def of step2_bar.py from step1_window.py

--- step1_window.py
+++ step1_window.py
@@ -1,15 +1,16 @@
 import pygame # To import pygame
-import os
 
-pygame.init() # start
-path = os.path.join("/ai", "develop", "/shooting","step2_bar.py")
-print(path)
-screen = pygame.display.set_mode((800, 600)) # Create Display size for High and width
+def window():
+    pygame.init()  # start
+    screen = pygame.display.set_mode((800, 600))  # Create Display size for High and width
+    pygame.display.set_caption("Shooting!")
 
-running = True
-while running: # like for sentence
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False #Stop running
+    running = True
+    while running:  # like for sentence
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False  # Stop running
+    running.quit()
 
-running.quit()
+if __name__ == "__main__":
+    window()
--- step2_bar.py
+++ step2_bar.py
@@ -1,22 +1,20 @@
 import pygame
-import os
-
+from step1_window import *
 pygame.init()
-win = os.path.join("/ai", "develop", "/shooting","step1_window.py")
-
-
-x = 800
-y = 600
+win = window()
 
 width = 20
 height = 20
 vel = 10
+
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
+    x = window()
+    y = window()
 
     if keys[pygame.K_LEFT] and x > 0:
         x -= vel
@@ -27,7 +25,7 @@ while run:
     if keys[pygame.K_DOWN] and y < 600 - height:
         y += vel
 
-    #win.fill((0, 0, 0))
+
     pygame.draw.rect(win,(255, 0, 0),(x, y, width, height))
     pygame.display.update()
 pygame.quit()
