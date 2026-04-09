955e5d8e8152aaf2dcf6df67057b63cfc32ac753
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-04-09 23:26:22 +0900

    anyway, commit

--- step2_bar.py
+++ step2_bar.py
@@ -1,21 +1,23 @@
 import pygame
+
 from step1_window import *
 def bar():
     pygame.init()
-    win = window()
+    win = window() # def window() from step1_window
 
-    x = 100
-    y = 100
+    x = 400
+    y = 540
 
-    width = 20
-    height = 20
+    width = 10
+    height = 10
     vel = 10
 
     run = True
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                run = False # If you change "False", you never finished
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT] and x > 0:
@@ -30,4 +32,7 @@ def bar():
         win.fill((0, 0, 0)) # Leave no trace of Bar
         pygame.draw.rect(win,(255, 0, 0),(x, y, width, height))
         pygame.display.update()
-    pygame.quit()
+
+pygame.init()
+if __name__ == "__main__":
+    bar()
