49c81c5befe85d967991e91fbf69e0985b4ea300
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-04-09 14:27:17 +0900

    add "def" of step2

--- step2_bar.py
+++ step2_bar.py
@@ -1,32 +1,33 @@
 import pygame
 from step1_window import *
-pygame.init()
-win = window()
+def bar():
+    pygame.init()
+    win = window()
 
-x = 100
-y = 100
+    x = 100
+    y = 100
 
-width = 20
-height = 20
-vel = 10
+    width = 20
+    height = 20
+    vel = 10
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    keys = pygame.key.get_pressed()
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > 0:
-        x -= vel
-    if keys[pygame.K_RIGHT] and x < 800 - width:
-        x += vel
-    if keys[pygame.K_UP] and y > 0:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < 600 - height:
-        y += vel
+        if keys[pygame.K_LEFT] and x > 0:
+            x -= vel
+        if keys[pygame.K_RIGHT] and x < 800 - width:
+            x += vel
+        if keys[pygame.K_UP] and y > 0:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < 600 - height:
+            y += vel
 
-    win.fill((0, 0, 0)) # Leave no trace of Bar
-    pygame.draw.rect(win,(255, 0, 0),(x, y, width, height))
-    pygame.display.update()
-pygame.quit()
+        win.fill((0, 0, 0)) # Leave no trace of Bar
+        pygame.draw.rect(win,(255, 0, 0),(x, y, width, height))
+        pygame.display.update()
+    pygame.quit()
